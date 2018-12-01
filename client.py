b813dbf053eef75c6d51bff20cd7ac166fcbf695
Vegar Westerlund <vegar.westerlund@paihealth.com>
2018-12-01 11:05:50 +0000

    More passwords

--- client.py
+++ client.py
@@ -18,7 +18,9 @@ def main():
     client.on_connect = on_connect
     client.on_message = on_message
 
-    client.connect("iot.eclipse.org", 1883, 60)
+    #client.connect("iot.eclipse.org", 1883, 60)
+    client.username_pw_set("raiom", "FjaseFlyndreFisk")
+    client.connect("kanskje.de", 1883, 60)
 
     # Blocking call that processes network traffic, dispatches callbacks and
     # handles reconnecting.
